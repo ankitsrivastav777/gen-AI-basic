e72e60625780b04bc86e9b1468b4b75b005d56b5
ankitsrivastav777 <anks.it2296@gmail.com>
2025-07-11 07:36:35 +0530

    created image and audio GenAI

--- chat-app/src/basic-chat/index.ts
+++ chat-app/src/basic-chat/index.ts
@@ -1,8 +1,14 @@
 import { OpenAI } from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
+import { encoding_for_model } from 'tiktoken';
+import { get } from 'http';
+
 
 const openai = new OpenAI()
+
+const encoding = encoding_for_model('gpt-4o');
+const MAX_TOKENS = 1000; // Maximum tokens for the response
 
 const context:OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
     role: 'system',
@@ -19,6 +25,11 @@ async function createChatCompletion() {
         role: "assistant",
         content: responseMessage.content
     });
+
+if (response.usage  && response.usage.total_tokens > MAX_TOKENS) {
+    deleteOlderMessages();
+}
+
     console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`);
 
 
@@ -37,3 +48,35 @@ process.stdin.on('data', async function (input) {
     await createChatCompletion();  
     
 })
+
+function deleteOlderMessages() {
+    let contextLength = context.length;
+    while (contextLength > MAX_TOKENS)
+    {
+        for (let i = 0; i < context.length; i++) {
+            const message = context[i];
+            if (message.role != 'system') {
+                context.splice(i, 1);
+                const currentTokenCount = getContextLength();
+                console.log(`Deleted system message. Current token count: ${currentTokenCount}`);
+                break
+            }
+        }      
+    }
+    }   
+
+function getContextLength() {
+    let totalTokens = 0;
+    context.forEach(message => {
+        if (typeof message.content === 'string') {
+            totalTokens += encoding.encode(message.content).length;
+        }   else if (Array.isArray(message.content)) {
+            message.content.forEach(item => {
+                if (item.type === 'text') {
+                    totalTokens += encoding.encode(item.text).length;
+                }
+            });
+        }
+    });
+    return totalTokens;
+}
